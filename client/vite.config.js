255751c2441a7718d37b8d16b518336a668613da
Ashley Hua <AshleyHua100@gmail.com>
2026-04-01 15:18:11 -0500

    fix deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/the-economist-reading-club/',
   server: {
     proxy: {
       '/api': { target: 'http://localhost:3001', changeOrigin: true },
